dad0bf2ebb9978aebbdb502055f5c4baf8c3f930
Monu-11 <monukumarsinghmonu@gmail.com>
2023-11-05 01:07:58 +0530

    database connection

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./db/database.js";
+
+const app = express();
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB();
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port : ${process.env.PORT}`);
+});
