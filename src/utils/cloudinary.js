dc0d43589f52226e46bb566467166364dd6104fb
Monu-11 <monukumarsinghmonu@gmail.com>
2023-12-12 00:22:44 +0530

    Register config

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -11,12 +11,13 @@ const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
     // upload the file on cloudinary
-    const response = await cloudinary.v2.uploader.upload(localFilePath, {
+    const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
     // file has been uploaded successfull
-    console.log(response);
-    console.log("file is uploaded on cloudinary", response.url);
+    // console.log(response);
+    // console.log("file is uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload  operation got failed
